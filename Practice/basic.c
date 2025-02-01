304b272b7292e23fbffe5bb4233f0627d938e4c2
SV-000 <vardhan0584@gmail.com>
2025-02-01 14:11:46 +0530

    c5

---
+++ Practice/basic.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World\n");
+    printf("This is my first program\n");
+    return 0;
+}
--- Problems/CC/luc7.java
+++ Problems/CC/luc7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class luc7 {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        String n=sc.nextLine();
         
+        sc.close();
     }
 }
